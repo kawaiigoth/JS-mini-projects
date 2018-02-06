8dc57acd7cbf36c6b2b85141cd82c8f478002687
kawaiigoth <nightknight92@mail.ru>
2018-02-06 13:25:59 +0400

    3d clock rotation and shadow

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,7 @@
     const MINUTES_ARROW = document.getElementById("Minutes");
     const HOURS_ARROW = document.getElementById("Hours");
     const BODY = document.getElementsByTagName("body")[0];
-
+    const CLOCK = document.getElementById("Clock");
     let timer = setTimeout(function tick() {
         let now = null;
         now = new Date();
@@ -36,9 +36,26 @@
 
 
     BODY.addEventListener("mousemove", function (e) {
-        console.log(e);
         let posX = e.clientX, posY = e.clientY;
-
+        let wid= window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+            hi= window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight ;
+        clockShadow(posX,posY,wid,hi)
     });
-    function clockShadow(){}
+    function clockShadow(posX,posY,wid,hi){
+        let [centerX,centerY] = [wid/2,hi/2];
+        let [centerPosX, centerPosY] = [posX - centerX, posY - centerY];
+        let half_width = centerX;
+        let half_height = centerY;
+        let max_offset = 25; //From border size;
+        let offset_segment = half_width/max_offset;
+        let coeficentX = max_offset/half_width;
+        let coeficentY = max_offset/half_height;
+        let shadowX = Math.floor(-centerPosX*coeficentX);
+        let shadowY = Math.floor(-  centerPosY*coeficentY);
+        console.log(shadowX, shadowY);
+        CLOCK.style.filter = "drop-shadow(" +shadowX+"px "+ shadowY +"px 20px red)";
+
+        CLOCK.style.transform = "rotateY("+  shadowX + "deg) rotateX(" + shadowY + "deg)";
+
+    }
 }());
